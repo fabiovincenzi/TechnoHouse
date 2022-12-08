da05aef29014f7c6a251289d893863de67bfaffe
Matteo <matteo.iorio01@gmail.com>
2022-12-08 11:50:42 +0100

    changed the title of base.php

--- base.php
+++ base.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
     <link href="style/style.css" rel="stylesheet"/>
-    <title>Home Page</title>
+    <title> TO SET </title>
 </head>
 <body>
 
