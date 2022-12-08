81ea7cee9c2d615e269176b3d0feeba6e4dcdda3
Matteo <matteo.iorio01@gmail.com>
2022-12-08 11:01:16 +0100

    added the icons for the menu

---
+++ base.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
+    <link href="style/style.css" rel="stylesheet"/>
+    
+    <title>Document</title>
+</head>
+<body>
+
+<nav class="navbar fixed-bottom navbar-expand bg-light justify-content-center">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="./view/home.php">
+                <i class="bi bi-house"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./view/search.php">SEARCH</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./view/notification.php">NOTIFICATION</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="./view/profile.php">PROFILE</a>
+        </li>
+    </ul>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
