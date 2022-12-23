35f15ecae213a0f1dbe9120cc677931023ad3197
Matteo <matteo.iorio01@gmail.com>
2022-12-23 10:34:06 +0100

    added all the references to the pages

--- controller_notification.php
+++ controller_notification.php
@@ -1,7 +1,7 @@
 <?php
 require_once './model/php/bootstrap.php';
 //Base Template
-$viewBag["title"] = "Search";
+$viewBag["title"] = "Notification";
 $viewBag["page"] = "./view/primary/notification.php";
 $viewBag["sript"] = array(
     "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
