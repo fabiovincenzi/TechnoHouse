7b823381f4aa2cc4f0760c8dbaf3179491933bd2
Matteo <matteo.iorio01@gmail.com>
2022-12-24 12:29:55 +0100

    fixed index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once './model/php/bootstrap.php';
 $viewBag["title"] = "Blog TW - Home";
 $viewBag["page"] = "./view/primary/feed.php";
 $viewBag["posts"] = 0; //
-$viewBag["sript"] = array(
+$viewBag["script"] = array(
     "https://code.jquery.com/jquery-3.3.1.slim.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
     "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
