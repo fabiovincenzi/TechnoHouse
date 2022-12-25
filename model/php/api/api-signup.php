56b2711aba3643349e38ce050851b2a03caadfd4
Matteo <matteo.iorio01@gmail.com>
2022-12-25 11:35:09 +0100

    added check for passwords

--- model/php/api/api-signup.php
+++ model/php/api/api-signup.php
@@ -1,10 +1,34 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/TechnoHouse/model/php/bootstrap.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/TechnoHouse/model/php/util/functions.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/model/php/util/functions.php';
 $result["logged"] = false;
 
 if(isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["residence"]) && isset($_POST["birthdate"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm-password"])){
-    var_dump($_POST);
+    $name = $_POST["name"];
+    $surname = $_POST["surname"];
+    if(containsNumber($name)){
+        $result["errorMSG"] = "Error : The parameter NAME contains one or more numbers";
+    }
+    if(containsNumber($surname)){
+        $result["errorMSG"] = "Error : The parameter SURNAME contains one or more numbers";
+    }
+    $date = $_POST["birthdate"];
+    if(!checkBirthdate($date)){
+        $result["errorMSG"] = "Error : In order to use this site you must be at least eighteen";
+    }
+    $email = $_POST["email"];
+    if(!checkEmail($email)){
+        $result["errorMSG"] = "Error : The Email's format is wrong. Did you forgot the @ ?";
+    }
+    $password = hash('whirlpool', $_POST["password"]);
+    $conf_password = hash('whirpool', $_POST["confirm-password"]);
+    if(!checkPasswords($password, $conf_password)){
+        $result["errorMSG"] = "Error : The passwords are not equal. Please insert again the passwords".$conf_password;
+    }
+    //$password = hash('sha512', $password.$salt); // codifica la password usando una chiave univoca.
+
+}else{
+    //Controllo quale parametro manca
 }
 header('Content-Type: application/json');
 echo json_encode($result);
--- model/php/util/functions.php
+++ model/php/util/functions.php
@@ -8,6 +8,19 @@ function containsNumber($input){
     return (preg_match('~[0-9]+~', $input));
 }
 
+function checkBirthdate($date){
+    $input_date = date_create_from_format('Y-m-d', $date);
+    $now = new DateTime();
+    return ($now->diff($input_date))->y >= 18;
+}
+
+function checkEmail($email){
+    return str_contains($email, "@");
+}
+
+function checkPasswords($password1, $password2){
+    return strcmp($password1, $password2) == 0;
+}
 function registerLoggedUser($user){
     $_SESSION["idUser"] = $user["email"];
     $_SESSION["email"] = $user["password"];
