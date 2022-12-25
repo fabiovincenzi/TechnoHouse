cab5040c7a7ccebab05dcf28b4ae17b272848b82
Matteo <matteo.iorio01@gmail.com>
2022-12-25 09:54:37 +0100

    added axios inside signup

--- controller_signup.php
+++ controller_signup.php
@@ -8,6 +8,7 @@ $viewBag["script"] = array(
     "https://code.jquery.com/jquery-3.3.1.slim.min.js",
     "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
     "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
+    "https://unpkg.com/axios/dist/axios.min.js",
     "./model/javascript/signup/signup.js"
 
 );
