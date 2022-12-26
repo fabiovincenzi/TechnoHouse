a4cb7272625d5c533b03cebffc05f7cd69712f01
Matteo <matteo.iorio01@gmail.com>
2022-12-26 16:58:20 +0100

    added the check for the user if he is logged in

--- controller_profile.php
+++ controller_profile.php
@@ -1,11 +1,15 @@
 <?php
 require_once './model/php/bootstrap.php';
 //Base Template
-$viewBag["title"] = "Profile";
-$viewBag["script"] = array(
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
-    "https://unpkg.com/axios/dist/axios.min.js",
-    "./model/javascript/profile/profile.js"
-);
-require './view/primary/base.php';
+if (isUserLoggedIn()) {
+    $viewBag["title"] = "Profile";
+    $viewBag["script"] = array(
+        "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
+        "https://unpkg.com/axios/dist/axios.min.js",
+        "./model/javascript/profile/profile.js"
+    );
+    require './view/primary/base.php';
+}else{
+    header("location: controller_login.php");
+}
 ?>  
