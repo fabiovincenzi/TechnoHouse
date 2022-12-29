e3b5c24546516eb1822259fc78c73522af4c5a96
Matteo <matteo.iorio01@gmail.com>
2022-12-29 12:54:26 +0100

    implemented the chat

--- model/javascript/message/single-chat.js
+++ model/javascript/message/single-chat.js
@@ -6,14 +6,43 @@ const main = document.querySelector('main');
 axios.get(`model/php/api/api-chat.php?idChat=${chatId}`).then(response=>{
     console.log(response);
     if (response.data["logged"]){
-        main.innerHTML = generateChat(response.data['destination'], response.data['chat']);
-        //aggiungo i messaggi
+        main.innerHTML = generateChat(response.data['destination']);
+        populateMessages(response.data['chat'], response.data['source']);
         addListener();
       } else {
-        //window.location.replace("./controller_login.php");   
+        window.location.replace("./controller_login.php");   
       }
     }
 );
+
+function populateMessages(messages, source){
+    messages.forEach(element => {
+        let div = "";
+        console.log(element);
+        if(element["me"]){
+            div = `
+                <div class="d-flex flex-row justify-content-end mb-4 pt-1">
+                    <div>
+                        <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">${element["body"]}</p>
+                        <p class="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">${element["data"]}</p>
+                    </div>
+                    <img class="profileImg" src="" alt="${source["name"]} ${source["surname"]} profile image" />
+                </div>`;
+        }else{
+            div = `
+                    <div class="d-flex flex-row justify-content-start">
+                        <img class="profileImg" src="" alt="${element["name"]} ${element["surname"]} profile image" />
+                            <div>
+                                <p class="small p-2 ms-3 mb-1 rounded-3 bg-light">${element["body"]}</p>
+                                <p class="small ms-3 mb-3 rounded-3 text-muted">${element["data"]}</p>
+                            </div>
+                    </div>`;
+                
+        }
+        document.getElementById("messages").innerHTML += div; 
+
+    });
+}
 
 function addListener(){
     let input = document.getElementById('input-msg');
@@ -31,12 +60,10 @@ function sendMessage(message){
     formData.append('idChat', chatId);
     axios.post('model/php/api/api-chat.php', formData).then(response => {
         console.log(response);
-        //aggiorno chat
     });
 }
 
 function generateChat(other_user, messages){
-    console.log(c);
     let chat = `
     <div class="container">
     <div class="row justify-content-center">
@@ -44,29 +71,13 @@ function generateChat(other_user, messages){
 
         <div class="card">
           <div class="card-header d-flex justify-content-between align-items-center p-3">
-            <h5 class="mb-0"></h5>
+            <h5 id="other-user" class="mb-0">${other_user["name"]} ${other_user["surname"]}</h5>
           </div>
-          <div class="card-body scroll">
-            <div class="d-flex flex-row justify-content-start">
-              <img class="profileImg" src=""
-                alt="ALTRO">
-              <div>
-                <p class="small p-2 ms-3 mb-1 rounded-3 bg-light">MESSAGGIO</p>
-                <p class="small ms-3 mb-3 rounded-3 text-muted">23:58</p>
-              </div>
+            <div id="messages" class="card-body scroll">
+
+
             </div>
 
-            <div class="d-flex flex-row justify-content-end mb-4 pt-1">
-              <div>
-                <p class="small p-2 me-3 mb-1 text-white rounded-3 bg-primary">MESSAGGII</p>
-                <p class="small me-3 mb-3 rounded-3 text-muted d-flex justify-content-end">00:06</p>
-              </div>
-              <img src=""
-                alt="SENDER" class="profileImg">
-            </div>
-
-
-          </div>
           <div class="card-footer d-flex justify-content-start align-items-center p-3">
             <label class="invisible" for="input-msg">Text a message</label>
             <input type="text" class="form-control form-control-lg" id="input-msg" placeholder="Type message">
--- model/php/api/api-chat.php
+++ model/php/api/api-chat.php
@@ -9,10 +9,18 @@ if(isUserLoggedIn()){
         $idChat = $_GET[TAG_CHAT_ID];
         $chat = $dbh->getChatById($idChat)[0];
         $destination= $chat[TAG_USER_CHAT_SOURCE] == $id ? $dbh->getUserByID($chat[TAG_USER_CHAT_DESTINATION])[0] : $dbh->getUserByID($chat[TAG_USER_CHAT_SOURCE])[0];
+        
         $chats[TAG_DESTINATION] = array(TAG_USER_ID => $destination[TAG_USER_ID], 
         TAG_USER_NAME => $destination[TAG_USER_NAME],
         TAG_USER_SURNAME => $destination[TAG_USER_SURNAME], 
         TAG_USER_IMAGE => "");
+        
+        $source = $dbh->getUserById($id)[0];
+        $chats[TAG_SOURCE] = array(TAG_USER_ID => $source[TAG_USER_ID], 
+        TAG_USER_NAME => $source[TAG_USER_NAME],
+        TAG_USER_SURNAME => $source[TAG_USER_SURNAME], 
+        TAG_USER_IMAGE => "");
+
         if(!isset($_POST[TAG_CHAT_BODY])){
             $chats[TAG_USER_SINGLE_CHAT] = array();
             $messages = $dbh->getChatMessages($idChat);
