e1d2556f66e35eda75522996be7f860433210738
Matteo <matteo.iorio01@gmail.com>
2022-12-30 10:04:07 +0100

    added inside all the images the path

--- model/php/api/api-search.php
+++ model/php/api/api-search.php
@@ -7,9 +7,19 @@ if(isUserLoggedIn()){
     $id = $_SESSION[TAG_USER_ID];
     
     $tags = $dbh->getUserPreference($id);
+    $ids = array();
     $posts = array();
     foreach($tags as $tag){
-        array_push($posts, $dbh->getRandomPostsOf($tag, N_RANDOM_POSTS)[0]);
+        //["path"] = getRelativeDirUserPost($post[0]["User_idUser"], $idPost).$images[$i]["path"];
+        $post_tags = $dbh->getRandomPostsOf($tag, N_RANDOM_POSTS)[0];
+        foreach($post_tags as $post){
+            if(!in_array($post[TAG_POST_ID], $ids)){
+                $image = $dbh->getPostImages($post[TAG_POST_ID])[0];
+                array_push($post, array(TAG_POST_PATH => getRelativeDirUserPost($post[TAG_POST_USER], $post[TAG_POST_ID]).$images[TAG_POST_PATH]);
+                array_push($ids, $post[TAG_POST_ID]);
+                array_push($posts, $post);
+            }
+        }
     }
     $result[TAG_SEARCH_POSTS] = $posts;
 }
