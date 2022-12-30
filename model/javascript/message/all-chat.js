7bd5afb1d982464f394651c3561e7d78557cf286
Matteo <matteo.iorio01@gmail.com>
2022-12-30 10:21:13 +0100

    fixed the chat list

--- model/javascript/message/all-chat.js
+++ model/javascript/message/all-chat.js
@@ -3,15 +3,33 @@ const main = document.querySelector('main');
 axios.get('model/php/api/api-allchat.php').then(response => {
     //console.log(response.data['all-chat']);
     if (response.data["logged"]) {
-        main.innerHTML = getAllChat(response.data['all-chat']);
+        main.innerHTML = generateBase();
+        getAllChat(response.data['all-chat']);
       } else {
         window.location.replace("./controller_login.php");   
       }
 });
 
+function generateBase(){
+    let base = `
+                <div class="row justify-content-center">
+                        <div class="col-md-10 col-lg-8 col-xl-6">
+                            <h5 class="font-weight-bold mb-3 text-center text-lg-start">Chat</h5>
+                            <div class="card">
+                                <div class="card-body">
+                                    <ul id="all-chat" class="list-unstyled mb-0">
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+    return base;
+}
 
 
 function getAllChat(chats){
+    const ul = document.getElementById("all-chat");
     let content = "";
     chats.forEach(chat => {
         console.log(chat);
@@ -41,5 +59,5 @@ function getAllChat(chats){
                     </div>`;
         content += single_chat
     });
-    return content;
+    ul.innerHTML = content;
 }
