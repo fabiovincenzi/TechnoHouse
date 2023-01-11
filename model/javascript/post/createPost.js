6fced5cb8139530f1a83489326e7fcb7ea6eb7be
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2023-01-11 14:48:20 +0100

    accessibility of create post

--- model/javascript/post/createPost.js
+++ model/javascript/post/createPost.js
@@ -32,7 +32,7 @@ function generateForm(){
                         <div class="modal-body">
                             <form>
                                 <div class="form-group">
-                                    <label for="exampleFormControlTextarea1">Insert the new Tag here:</label>
+                                    <label for="newTag">Insert the new Tag here:</label>
                                     <input class="form-control" id="newTag" rows="3"></input>
                                 </div>
                             </form>
@@ -76,34 +76,7 @@ function generateForm(){
                             
                 </div>
             </div>
-            <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalLabel">Crop image</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">×</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="img-container">
-                            <div class="row">
-                                <div class="col-md-8">  
-                                    <!--  default image where we will set the src via jquery-->
-                                    <img class="img-crop" id="image">
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="preview"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                        <button type="button" class="btn btn-primary" id="crop">Crop</button>
-                    </div>
-                </div>
-            </div>
+            
         </div>
 
     </div>
